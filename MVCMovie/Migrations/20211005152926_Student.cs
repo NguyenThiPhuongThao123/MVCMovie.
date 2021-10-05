d7128a6c0f40e46e012ffa0fd193f07e3fc72763
Nguyen Thi Phuong Thao <phuongthaoov0408@gmail.com>
2021-10-06 06:43:13 +0700

    Add project files.

---
+++ MVCMovie/Migrations/20211005152926_Student.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCMovie.Migrations
+{
+    public partial class Student : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Tensv = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Tensv);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
---
+++ MVCMovie/Models/MvcMovieContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MVCMovie.Models
+{
+    internal class MvcMovieContext : IDisposable
+    {
+        private object p;
+
+        public MvcMovieContext(object p)
+        {
+            this.p = p;
+        }
+
+        public object Movie { get; internal set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
